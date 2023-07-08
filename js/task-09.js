c2caa52973b69d8e2edb5ecd53759c2702577e09
Evhenii Kolomoitsev <kolomoycev81@gmail.com>
2023-07-09 00:24:49 +0300

    10done

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const bodyEl = document.querySelector("body");
+const btnEl = document.querySelector(".change-color");
+const spanEl = document.querySelector(".color");
+
+btnEl.addEventListener("click", onClick);
+
+function onClick(event) {
+  const bodyColor = getRandomHexColor();
+  spanEl.textContent = String(bodyColor);
+  bodyEl.style.backgroundColor = String(bodyColor);
+}
--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,36 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  inputEl: document.querySelector("#controls > input"),
+  btnCreateEl: document.querySelector("button[data-create]"),
+  btnDestroyEl: document.querySelector("button[data-destroy]"),
+  divBoxesEl: document.querySelector("#boxes"),
+};
+
+refs.btnCreateEl.addEventListener("click", onClick);
+refs.btnDestroyEl.addEventListener("click", onClick);
+
+function onClick(event) {
+  const amount = refs.inputEl.valueAsNumber;
+  if (event.currentTarget.hasAttribute("data-create") && amount) {
+    createBoxes(amount);
+  } else {
+    destroyBoxes();
+  }
+}
+
+function createBoxes(amount) {
+  let markup = "";
+  for (let i = 1; i <= amount; i += 1) {
+    markup += `<div style=" width: ${20 + 10 * i}px; height: ${
+      20 + 10 * i
+    }px; background-color: ${getRandomHexColor()}"></div> `;
+  }
+  refs.divBoxesEl.innerHTML = markup;
+}
+
+function destroyBoxes() {
+  refs.divBoxesEl.innerHTML = "";
+}
